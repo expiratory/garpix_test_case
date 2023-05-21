69a2e936fc4052c0f6b8b29a867953c272817b85
Aleksandr_Kilchevskiy <alexkill94@mail.ru>
2023-05-21 09:11:01 +0300

    still in progress

--- app/Photos/urls.py
+++ app/Photos/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('api/v1/photos/gallery/', views.gallery, name='gallery'),
-    path('api/v1/photos/photo/<str:pk>/', views.viewPhoto, name='photo'),
-    path('api/v1/photos/add/', views.addPhoto, name='add'),
+    path('api/v1/photos/photo/<str:pk>/', views.view_photo, name='photo'),
+    path('api/v1/photos/add/', views.add_photo, name='add'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/Photos/views.py
+++ app/Photos/views.py
@@ -1,15 +1,35 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from .models import Photo
+
 
 def gallery(request):
-    return render(request, 'photos/gallery.html')
+    photos = Photo.objects.all()
+    context = {
+        'photos': photos,
+    }
+    return render(request, 'photos/gallery.html', context)
 
 
-def viewPhoto(request, pk):
-    return render(request, 'photos/photo.html')
+def view_photo(request, pk):
+    photo = Photo.objects.get(id=pk)
+    photo.count_of_views += 1
+    photo.save()
+    return render(request, 'photos/photo.html', {'photo': photo})
 
 
-def addPhoto(request):
+def add_photo(request):
+
+    if request.method == 'POST':
+        data = request.POST
+        image = request.FILES.get('image')
+
+        photo = Photo.objects.create(
+            title=data['title'],
+            description=data['description'],
+            image=image
+        )
+        return redirect('gallery')
+
     return render(request, 'photos/add.html')
